7daf22f5610719b09415e7e63f7625c0f3d15b5b
nikita <rashkevich2001@mail.ru>
2023-12-15 21:46:58 +0300

    'сменяемая-тема'

--- js/past.js
+++ js/past.js
@@ -122,19 +122,3 @@ function themeFile () {
 }
 themeFile ();
 
-// themeFile ();
-// let root = document.querySelector('.light');
-// function themeFile() {
-//   document.querySelector('.theme').addEventListener('click', () => {
-//     for (let i = 0; i < root.length; i++) {
-//       if (root[i].classList.contains('light')) {
-//         root[i].classList.remove('light');
-//         root[i].classList.add('dark');
-//       } else if (root[i].classList.contains('dark')) {
-//         root[i].classList.remove('dark');
-//         root[i].classList.add('light');
-//       }
-//     }
-//   })
-// }
-// themeFile();
